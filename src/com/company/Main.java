69945b244d0f7666adab263d4f062a9cd63d48f0
Rajanikant <rajanikantpawar31@gmail.com>
2021-10-11 23:35:46 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+String a="rajanikant";
+        System.out.println(a.length());
+        System.out.println(a.charAt(3));
+
+    }
+}
+
+//hA PROGRAM BROBR AAHE
